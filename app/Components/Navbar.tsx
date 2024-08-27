250495aeaac82cdccafe9a54b7762687c1e15549
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-08-27 15:33:18 +0545

    using accernity navbar and utils class to build navbar of our application

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React, { useState } from "react";
+import {
+  HoveredLink,
+  Menu,
+  MenuItem,
+  ProductItem,
+} from "@/app/Components/ui/navbar-menu";
+import { cn } from "@/app/lib/utils";
+import Link from "next/link";
+
+const Navbar = ({ className }: { className?: string }) => {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href="/">
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+        <MenuItem setActive={setActive} active={active} item="Our Courses">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/courses">All Courses</HoveredLink>
+            <HoveredLink href="/basic">Basic programming</HoveredLink>
+            <HoveredLink href="/advanced">Advanced Programming</HoveredLink>
+            <HoveredLink href="/contentwriting">Content Writing</HoveredLink>
+            <HoveredLink href="/softwareproduction">
+              Software Production
+            </HoveredLink>
+          </div>
+        </MenuItem>
+        <Link href={"contact"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Contact Us"
+          ></MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
+};
+
+export default Navbar;
