9ad247dc6ab3cff7e404c525f5bb3bc901b6beb0
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-08-29 22:46:35 +0545

    added featured card to page

--- app/Components/index.ts
+++ app/Components/index.ts
@@ -1,5 +1,9 @@
 import Navbar from "./Navbar";
 import HeroSection from "./HeroSection";
 import FeaturedSection from "./FeaturedSection";
+import WhyChooseUs from "./WhyChooseUs";
+import TestinomialCard from "./TestinomialCard";
 
-export { Navbar, HeroSection, FeaturedSection }
+
+
+export { Navbar, HeroSection, FeaturedSection, WhyChooseUs, TestinomialCard }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import { HeroSection, FeaturedSection } from "./Components";
+"use client";
+import {
+  HeroSection,
+  FeaturedSection,
+  WhyChooseUs,
+  TestinomialCard,
+} from "./Components";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedSection />
+      <WhyChooseUs />
+      <TestinomialCard />
     </main>
   );
 }
