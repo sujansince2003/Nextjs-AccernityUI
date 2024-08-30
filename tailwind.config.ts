20d338228f5eb9bbd71727907d4914ecfee81df2
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-08-30 15:14:00 +0545

    setup tailwind config for meteor effect

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,7 @@ const config: Config = {
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
         scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+        "meteor-effect": "meteor 5s linear infinite",
       },
       keyframes: {
         spotlight: {
@@ -44,7 +45,16 @@ const config: Config = {
             transform: "translate(calc(-50% - 0.5rem))",
           },
         },
+        meteor: {
+          "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
+          "70%": { opacity: "1" },
+          "100%": {
+            transform: "rotate(215deg) translateX(-500px)",
+            opacity: "0",
+          },
+        },
       },
+
     },
   },
   plugins: [addVariablesForColors,
