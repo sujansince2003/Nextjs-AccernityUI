c1ced42799e3931f0c264917efcd2a0dc0623f99
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2024-08-30 15:14:18 +0545

    typo fixed

--- app/Components/HeroSection.tsx
+++ app/Components/HeroSection.tsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
           fill="white"
         />
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text  bg-gradient-to-b from-neutral-50 to-neutral-400">
-          Master programming with the Industry Experts
+          Master Music with the Industry Experts
         </h1>
         <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
           Dive into our comprehensive music courses and transform your musical
